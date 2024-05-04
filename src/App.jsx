b7d6e59aa2cb064b5f9373e3b6aa95458bfc1cb7
Sergey Veremiychuk <veremey4uk@gmail.com>
2024-05-04 12:47:15 +0200

    Added styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,45 @@
-import { useState } from "react"
+import { useState, useRef, useEffect } from "react"
 import { makeRequest } from "./api/makeRequest"
-import "./App.css"
+import Markdown from "react-markdown"
+
+import "./App.scss"
 
 function App() {
 	const [data, setData] = useState()
 	const [message, setMessage] = useState("")
 	const [chat, setChat] = useState([])
+	const [loading, setLoading] = useState(false)
+	const [scroll, setScroll] = useState(false)
+
+	const testRef = useRef(null)
+	const scrollToElement = () => testRef.current.scrollIntoView()
+
+	useEffect(() => {
+		if (scroll) {
+			scrollToElement()
+		}
+
+		console.log("scroll - ", scroll) // TODO: remove
+	}, [scroll])
 
 	const askChatGPT = async (e) => {
 		e.preventDefault()
 
+		setLoading(true)
+		setScroll(true)
+
 		setChat((state) => [
 			...state,
 			{
-				role: "Admin",
+				role: "admin",
 				message,
 			},
 		])
 
 		setMessage("")
+
 		const response = await makeRequest(message)
+
 		setData(response)
 
 		setChat((state) => [
@@ -29,24 +49,36 @@ function App() {
 				message: response.content,
 			},
 		])
+
+		setLoading(false)
+		setScroll(false)
 	}
 
 	return (
-		<div>
-			<div>
+		<div className='chat'>
+			<div className='chat__messages'>
 				{chat.map((item, index) => (
-					<div key={index}>
-						<div>
-							<div>{item.role}</div>
-							<div>{item.message}</div>
+					<div
+						key={index}
+						ref={testRef}
+						className={`chat__item chat__item--${item.role}`}>
+						<div className={`message`}>
+							<h3 className='message__title'>{item.role}</h3>
+							<Markdown className='message__desc'>{item.message}</Markdown>
 						</div>
 					</div>
 				))}
-				{JSON.stringify(data, null, 2)}
 			</div>
-			<form onSubmit={askChatGPT}>
-				<input type='text' value={message} onChange={(e) => setMessage(e.target.value)} />
-				<button type='submit'>Send</button>
+			<form className='chat-form' onSubmit={askChatGPT}>
+				<input
+					className='chat-form__input'
+					type='text'
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
+				/>
+				<button className='chat-form__btn chat-form__btn--submit' type='submit'>
+					Send
+				</button>
 			</form>
 		</div>
 	)
